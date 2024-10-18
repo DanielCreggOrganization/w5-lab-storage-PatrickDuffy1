aa1b868203a105c7d00b7a0f0b8c50e684f745f4
Patrick Duffy <101258287+PatrickDuffy1@users.noreply.github.com>
2024-10-18 09:19:59 +0000

    Completed Task 4. Creating a Storage Service

--- w5-lab-storage/src/app/home/home.page.ts
+++ w5-lab-storage/src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 // src/app/home/home.page.ts
 import { Component } from '@angular/core';
-import { Storage } from '@ionic/storage-angular';
+import { StorageService } from '../services/storage.service';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 
@@ -16,67 +16,83 @@ export class HomePage {
   value: string = '';
   output: string = '';
 
-  constructor(private storage: Storage) {
-    storage.create();
-  }
+  constructor(private storageService: StorageService) {}
 
   async setItem() {
     try {
-      await this.storage.set(this.key, this.value);
+      await this.storageService.set(this.key, this.value);
       this.output = `Set ${this.key}: ${this.value}`;
     } catch (error) {
       console.error('Error setting item', error);
       this.output = `Error setting item: ${error}`;
+      throw error;
     }
   }
 
   async getItem() {
     try {
-      const value = await this.storage.get(this.key);
+      const value = await this.storageService.get(this.key);
+
+      if(await this.storageService.exists(this.key) == false)
+      {
+        throw new Error(this.key + " does not exist");
+      }
+
       this.output = `Get ${this.key}: ${value}`;
     } catch (error) {
       console.error('Error getting item', error);
       this.output = `Error getting item: ${error}`;
+      throw error;
     }
   }
 
   async clear() {
     try {
-      await this.storage.clear;
+      await this.storageService.clear();
       this.output = `Cleared`;
     } catch (error) {
       console.error('Error clearing', error);
       this.output = `Error clearing: ${error}`;
+      throw error;
     }
   }
 
   async remove() {
     try {
-      await this.storage.remove(this.key);
+      await this.storageService.remove(this.key);
+
+      if(await this.storageService.exists(this.key) == false)
+      {
+        throw new Error(this.key + " does not exist");
+      }
+
       this.output = `Removed ${this.key}: ${this.value}`;
     } catch (error) {
       console.error('Error removing item', error);
       this.output = `Error removing item: ${error}`;
+      
     }
   }
 
   async keys() {
     try {
-      const keys = await this.storage.keys();
+      const keys = await this.storageService.keys();
       this.output = `Keys: ${keys}`;
     } catch (error) {
       console.error('Error getting keys', error);
       this.output = `Error getting keys: ${error}`;
+      throw error;
     }
   }
 
   async length() {
     try {
-      const length = await this.storage.length();
+      const length = await this.storageService.length();
       this.output = `Length: ${length}`;
     } catch (error) {
       console.error('Error getting length', error);
       this.output = `Error getting length: ${error}`;
+      throw error;
     }
   }
 
@@ -85,17 +101,26 @@ export class HomePage {
     this.output = "";
 
     try {
-      this.storage.forEach((key, value, index) => {
+      this.storageService.forEach((key, value, index) => {
         this.output += key + ": " + value + ", ";
       });
     } catch (error) {
       console.error('Error getting items', error);
       this.output = `Error getting items: ${error}`;
+      throw error;
+    }
+  }
+
+  async exists() {
+    try {
+      const exists = await this.storageService.exists(this.key);
+      this.output = `Exists: ${exists}`;
+    } catch (error) {
+      console.error('Error checking if exists', error);
+      this.output = `Error checking if exists: ${error}`;
+      throw error;
     }
   }
 
 }
 
-function callback(value: any, key: string, iterationNumber: Number) {
-  throw new Error('Function not implemented.');
-}
